97445d10a132179aba7f71eaa880fc287da468c9
tyler13313 <tyler13313@naver.com>
2021-12-28 21:12:33 +0900

    프로젝트 초기화

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import { createServer, Server } from "http";
+
+const app = express();
+
+const server = createServer(app);
+
+server.listen(process.env.PORT || 5000);
