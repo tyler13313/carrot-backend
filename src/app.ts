eb8316322fef92ac883dd655fc57d489bedadef3
tyler13313 <tyler13313@naver.com>
2021-12-30 21:55:54 +0900

    서버를 통해 데이터 추가 해보기

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import { createServer, Server } from "http";
+import bodyParser from "body-parser";
 
 import controller from "./controller";
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(controller);
 
 const server = createServer(app);
-
 server.listen(process.env.PORT || 5000);
--- src/controller/school.controller.ts
+++ src/controller/school.controller.ts
@@ -1,14 +1,42 @@
 import express from "express";
+
+type School = {
+  id: number;
+  name: string;
+};
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  const data = [
-    {
-      id: 1,
-      name: "동북고",
-    },
-  ];
-  return res.status(200).json(data);
+const data: School[] = [
+  {
+    id: 1,
+    name: "동북고",
+  },
+];
+
+router.get("/", (req, res) => res.status(200).json(data));
+
+router.get("/:schoolId", (req, res) => {
+  const { schoolId } = req.params;
+  if (!schoolId) {
+    return res.status(400).json();
+  }
+
+  const schoolIdNumber = parseInt(schoolId, 10);
+  if (!data.some(({ id }) => id === schoolIdNumber)) {
+    return res.status(404).json();
+  }
+  const filtered = data.filter((item) => item.id === schoolIdNumber);
+  return res.status(200).json(filtered[0]);
+});
+
+router.post("/", (req, res) => {
+  const school: School = req.body as School;
+  if (!school) {
+    return res.status(400).json();
+  }
+  data.push(school);
+  return res.status(201).json();
 });
 
 export default router;
