9f517c7f2a16d2a1157198cffab928f22c29dc70
elliot0413 <elliot0413@naver.com>
2022-01-06 21:54:31 +0900

    데이터 입력,받아오기

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import schoolController from "./school.controller";
+import tradeController from "./trade.controller";
 
 const router = express.Router();
 
 router.use("/schools", schoolController);
+router.use("/trade", tradeController);
 
 export default router;
